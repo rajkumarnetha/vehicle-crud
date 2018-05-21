ebc98adb1f08fe652c7e7309cb8fac2c3b81e70c
Rajkumar Pallae <rajkumarnetha@gmail.com>
2018-05-21 13:42:28 -0700

    Removing target files

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-insert into vehicle
-values(1,'Car', 'civic');
-
-insert into vehicle
-values(2,'Car', 'altima');
--- target/classes/schema.sql
+++
@@ -1,6 +0,0 @@
-create table Vehicle
-(
-   id integer not null auto_increment,
-   type varchar(255) not null,
-   name varchar(255) not null   
-);
